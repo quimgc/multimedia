d5632c0f0a35477456ac4797edadb80618da081a
quimgc <quimgonzalez@iesebre.com>
2018-04-19 00:55:13 +0200

    Sync

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-Vue.use(Router)
+import GameComponent from '@/components/GameComponent'
+import HelloWorld from '@/components/HelloWorld'
+import ClockComponent from '@/components/ClockComponent'
 
-const HelloWorld = () => import('@/components/HelloWorld')
-const ClockComponent = () => import('@/components/ClockComponent')
-const GameComponent = () => import('@/components/GameComponent')
+Vue.use(Router)
+// const HelloWorld = () => import('@/components/HelloWorld')
+// const ClockComponent = () => import('@/components/ClockComponent')
+// const GameComponent = () => import('@/components/GameComponent')
 
 export default new Router({
   routes: [
