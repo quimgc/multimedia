54aeeda4128a0dffa9270d9f83ce1b1b5a979f45
quimgc <quimgonzalez@iesebre.com>
2018-04-20 23:20:06 +0200

    Sync

--- src/components/PaintComponent.vue
+++ src/components/PaintComponent.vue
@@ -1,71 +1,109 @@
 <template>
     <div id="content">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style media="screen">canvas{border:1px solid #ccc}</style>
         <canvas id="example" height=450 width=300></canvas>
     </div>
 </template>
-
-<style>
-
-</style>
-
-<!--<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
-
-<!--<script>-->
-<!--export default {-->
-  <!--name: 'Paint',-->
-  <!--data () {-->
-    <!--return {-->
-      <!--CanvasDrawr: function (options) {-->
-        <!--var canvas, lines, offset, self, id, colors-->
-        <!--canvas = document.getElementById(options.id), ctxt = canvas.getContext('2d')-->
-        <!--canvas.style.width = '100%'-->
-        <!--canvas.width = canvas.offsetWidth-->
-        <!--canvas.style.width = ''-->
-        <!--ctxt.lineWidth = options.size || Math.ceil(Math.random() * 35)-->
-        <!--ctxt.lineCap = options.lineCap || 'round'-->
-        <!--ctxt.pX = undefined-->
-        <!--ctxt.pY = undefined-->
-        <!--var lines = [ , , ]-->
-        <!--var offset = $(canvas).offset()-->
-        <!--var self = {init: function () {-->
-          <!--canvas.addEventListener('touchstart', self.preDraw, false)-->
-          <!--canvas.addEventListener('touchmove', self.draw, false)-->
-        <!--},-->
-        <!--preDraw: function (event) {-->
-          <!--$.each(event.touches, function (i, touch) {-->
-            <!--id = touch.identifier-->
-            <!--colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'orangered'], mycolor = colors[Math.floor(Math.random() * colors.length)]-->
-            <!--lines[id] = {x: this.pageX - offset.left, y: this.pageY - offset.top, color: mycolor}-->
-          <!--})-->
-          <!--event.preventDefault()-->
-        <!--},-->
-        <!--draw: function (event) {-->
-          <!--var e = event, hmm = {}-->
-          <!--$.each(event.touches, function (i, touch) {-->
-            <!--var id = touch.identifier, moveX = this.pageX - offset.left - lines[id].x, moveY = this.pageY - offset.top - lines[id].y-->
-            <!--var ret = self.move(id, moveX, moveY)-->
-            <!--lines[id].x = ret.x-->
-            <!--lines[id].y = ret.y-->
-          <!--})-->
-          <!--event.preventDefault()-->
-        <!--},-->
-        <!--move: function (i, changeX, changeY) {-->
-          <!--ctxt.strokeStyle = lines[i].color-->
-          <!--ctxt.beginPath()-->
-          <!--ctxt.moveTo(lines[i].x, lines[i].y)-->
-          <!--ctxt.lineTo(lines[i].x + changeX, lines[i].y + changeY)-->
-          <!--ctxt.stroke()-->
-          <!--ctxt.closePath()-->
-          <!--return {x: lines[i].x + changeX,y: lines[i].y + changeY}-->
-        <!--}}-->
-        <!--return self.init()-->
-      <!--}-->
-    <!--}-->
-  <!--},-->
-  <!--mounted () {-->
-    <!--console.log('Mounted ok')-->
-  <!--}-->
-<!--}-->
-<!--</script>-->
+<style media="screen">canvas{border:1px solid #ccc}</style>
+<script>
+  // prèviament he fet un npm install jquery
+  import $ from 'jquery'
+  export default {
+    name: 'Paint',
+    data () {
+      return {
+        paintWithClick: false,
+        CanvasDrawr: function (options) {
+          var canvas, lines, offset, self, id, colors, ctxt, mycolor
+          canvas = document.getElementById(options.id)
+          ctxt = canvas.getContext('2d')
+          canvas.style.width = '100%'
+          canvas.width = canvas.offsetWidth
+          canvas.style.width = ''
+          ctxt.lineWidth = options.size || Math.ceil(Math.random() * 35)
+          ctxt.lineCap = options.lineCap || 'round'
+          ctxt.pX = undefined
+          ctxt.pY = undefined
+          //          lines = [ , , ]
+          lines = []
+          offset = $(canvas).offset()
+          self = {init: function () {
+            canvas.addEventListener('touchstart', self.preDraw, false)
+            canvas.addEventListener('mousedown', self.preDrawClick, false)
+            canvas.addEventListener('mouseup', self.stopDrawClick, false)
+            canvas.addEventListener('touchmove', self.draw, false)
+            canvas.addEventListener('mousemove', self.drawClick, false)
+          },
+          preDraw: function (event) {
+            console.log(event)
+            $.each(event.touches, function (i, touch) {
+              id = touch.identifier
+              colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'orangered']
+              mycolor = colors[Math.floor(Math.random() * colors.length)]
+              lines[id] = {x: this.pageX - offset.left, y: this.pageY - offset.top, color: mycolor}
+            })
+            event.preventDefault()
+          },
+          draw: function (event) {
+            var id, ret, moveX, moveY
+            $.each(event.touches, function (i, touch) {
+              id = touch.identifier
+              moveX = this.pageX - offset.left - lines[id].x
+              moveY = this.pageY - offset.top - lines[id].y
+              ret = self.move(id, moveX, moveY)
+              lines[id].x = ret.x
+              lines[id].y = ret.y
+            })
+            event.preventDefault()
+          },
+          preDrawClick: function (event) {
+            this.paintWithClick = true
+            colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'orangered']
+            mycolor = colors[Math.floor(Math.random() * colors.length)]
+            lines = {x: event.pageX - offset.left, y: event.pageY - offset.top, color: mycolor}
+            console.log(lines[0])
+            event.preventDefault()
+          },
+          drawClick: function (event) {
+            if (this.paintWithClick) {
+              var ret, moveX, moveY
+              moveX = event.pageX - offset.left - lines.x
+              moveY = event.pageY - offset.top - lines.y
+              ret = self.moveClick(moveX, moveY)
+              lines.x = ret.x
+              lines.y = ret.y
+              event.preventDefault()
+            }
+          },
+          stopDrawClick: function () {
+            this.paintWithClick = false
+          },
+          move: function (i, changeX, changeY) {
+            ctxt.strokeStyle = lines[i].color
+            ctxt.beginPath()
+            ctxt.moveTo(lines[i].x, lines[i].y)
+            ctxt.lineTo(lines[i].x + changeX, lines[i].y + changeY)
+            ctxt.stroke()
+            ctxt.closePath()
+            return {x: lines[i].x + changeX, y: lines[i].y + changeY}
+          },
+          moveClick: function (changeX, changeY) {
+            ctxt.strokeStyle = lines.color
+            ctxt.beginPath()
+            ctxt.moveTo(lines.x, lines.y)
+            ctxt.lineTo(lines.x + changeX, lines.y + changeY)
+            ctxt.stroke()
+            ctxt.closePath()
+            return {x: lines.x + changeX, y: lines.y + changeY}
+          }
+          }
+          return self.init()
+        }
+      }
+    },
+    mounted () {
+      console.log('Mounted ok')
+      this.CanvasDrawr({id: 'example', size: 15})
+    }
+}
+</script>
